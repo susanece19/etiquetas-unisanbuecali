eb4ff95df0a09d5ce0822066a2be3f37d3a3fd2e
susanece19 <susanece19@gmail.com>
2026-08-05 11:30:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ st.sidebar.markdown(
     """
     <div style="text-align: center; color: #4B5563; font-size: 0.85rem; padding: 10px; background-color: #F3F4F6; border-radius: 8px;">
         <b>💡 Aplicación Creada Por:</b><br>
-        <strong>SUSAN ESMERALDA CABRERA ENRIQUEZ/ UNIVERSIDAD DE SAN BUENAVENTURA</strong><br>
+        <strong>SUSAN CABRERA ENRIQUEZ/ UNIVERSIDAD DE SAN BUENAVENTURA</strong><br>
         <span style="font-size: 0.75rem; color: #6B7280;">© 2026 Todos los derechos reservados</span><br>
         <small>Contacto: susanece19@gmail.com o laboratorista.biologia@usbcali.edu.co</small>
     </div>
